b553cb4451923645e3c5f2c5cd114bffca1766d7
LucyWeltner <lucyiweltner@gmail.com>
2020-02-29 21:52:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+
+function map(array, callback) {
+	let newArray = []
+	for (let i = 0; i < array.length; i++) {
+		newArray.push(callback(array[i]))
+	}
+	return newArray
+}
+
+function reduce(array, callback, start=0) {
+	if (typeof(callback(array[0], start)) === "boolean"){
+		start = true
+	}
+	for (let i = 0; i < array.length; i++){
+		start = callback(array[i], start)
+	}
+	return start
+}
